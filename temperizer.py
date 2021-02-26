dc9d440c94d51d03caf0018ec960a1cb11849ca0
tamjid-ahsan <74892015+tamjid-ahsan@users.noreply.github.com>
2021-02-26 15:48:45 -0500

    done project

--- temperizer.py
+++ temperizer.py
@@ -7,5 +7,34 @@ def convert_f_to_c(temperature_f):
 
 
 def convert_c_to_f(temperature_c):
-    """YOUR DOCSTRING HERE."""
-    pass
+    """Convert Celsius to Fahrenheit."""
+    temperature_f = (temperature_c * 9/5) + 32
+    return temperature_f
+
+
+def convert_c_to_k(temperature_c):
+    """Convert Celsius to Kelvin."""
+    temperature_k = temperature_c + 273.15
+    return temperature_k
+
+
+def convert_f_to_k(temperature_f):
+    """Convert Fahrenheit to Kelvin."""
+    temperature_k = temperature_f + 459.67
+    return temperature_k
+
+
+def convert_k_to_c(temperature_k):
+    """Convert Kelvin to Celsius."""
+    temperature_c = temperature_k - 273.15
+    return temperature_c
+
+def convert_k_to_f(temperature_k):
+    """Convert Kelvin to Fahrenheit."""
+    temperature_f = (temperature_k - 273.15) * (9/5) + 32
+    return temperature_f
+
+def convert_f_to_all(temperature_f):
+    temp_c = convert_f_to_c(temperature_f)
+    temp_k = convert_f_to_k(temperature_f)
+    print(f'The temperature [number] F is\n\t- {temp_c} in C\n\t- {temp_k} in k')
